2b08a11577fff3e493a30f4eef2fc912dd45c60e
r2sQj*jKp6L.2Q4 <divinkuate01@gmail.com>
2025-07-14 19:33:35 +0100

    les variables (number, string, list)

---
+++ jour1.py
@@ -0,0 +1,24 @@
+#VARIABLE 
+#EXAMPLE OF DECLARATION 
+#integer
+
+a=50
+b=10
+c=a/b 
+d=5555564
+
+print (c)
+
+#string
+
+x="john doe"
+y="doe  is the younger in the family"
+
+print(x[0])
+
+#list
+
+number=[1, 2 , 4]
+
+
+print(number[0])
