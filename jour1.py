9f2cd525cac61ffdb339c91f2a5ecadf456cb013
r2sQj*jKp6L.2Q4 <divinkuate01@gmail.com>
2025-07-14 20:46:10 +0100

    les boucles  et les listes

--- jour1.py
+++ jour1.py
@@ -18,7 +18,64 @@ print(x[0])
 
 #list
 
-number=[1, 2 , 4]
+squares=[1, 9 , 4]
+print(squares)
+fruts=['pawpaw', 'plum', 'orange']
+
+#operation on list 
+#to add
+fruts.append('kiwi')
+
+print(fruts)
+
+#del(): enlever a parti de lindex
+#remove(): enlever un element d'une liste
+# len() : obtenir la taille de la liste
+# insert(index, élément) : insérer un élément à un index donné
+# pop() : retirer et retourner le dernier élément (ou un élément à un index donné)
+
+m, n= 0, 1
+while m<10 :
+    print(m)
+    m, n = n, m+n
 
 
-print(number[0])
+#Boucles
+# if ..elif .. else
+
+if a<b:
+    print(str(a)+'est plus petit que'+str(b))
+elif a==b :
+    print(f"{a} est egal a {b}")
+else :
+    print('ras')
+
+# for in
+
+for p in fruts:
+    print(p, len(p))
+
+# Create a sample collection
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+# Strategy:  Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+    else :
+        print(users)
+
+# Strategy:  Create a new collection
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+
+# range
+for i in range(5):
+     print(i)
+
+#break
+
+
+
